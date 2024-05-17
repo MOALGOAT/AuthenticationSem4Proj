ae27a2404f4b73d1b54fdf3c3a140e1bd7b40b62
jakobhaulund <jakobhaulund@hotmail.com>
2024-05-17 13:05:19 +0200

    logging virker :-)

--- AuthenticationService/Controllers/Authenticationcontroller.cs
+++ AuthenticationService/Controllers/Authenticationcontroller.cs
@@ -10,23 +10,35 @@ using VaultSharp;
 using VaultSharp.V1.AuthMethods.Token;
 using VaultSharp.V1.AuthMethods;
 using VaultSharp.V1.Commons;
-
+using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using NLog;
 
 namespace Authentication.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class Authenticationcontroller : ControllerBase
+    public class AuthenticationController : ControllerBase
     {
-        private readonly ILogger<Authenticationcontroller> _logger;
+        private readonly ILogger<AuthenticationController> _logger;
         private readonly IConfiguration _config;
         private readonly VaultService _vaultService;
+        private static readonly Logger _nLogger = LogManager.GetCurrentClassLogger();
+        private string secret;
+        private string issuer;
 
-        public Authenticationcontroller(ILogger<Authenticationcontroller> logger, IConfiguration config, VaultService vault)
+        public AuthenticationController(ILogger<AuthenticationController> logger, IConfiguration config, VaultService vault)
         {
             _config = config;
             _logger = logger;
             _vaultService = vault;
+
+            // Hent hemmeligheden og udstederen fra Vault
+            secret = _vaultService.GetSecretAsync("secrets", "SecretKey").ToString();
+            issuer = _vaultService.GetSecretAsync("secrets", "IssuerKey").ToString();
+
         }
 
         private string GenerateJwtToken(string username, string issuer, string secret)
@@ -62,18 +74,33 @@ namespace Authentication.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginModel login)
         {
+            //lav test og logging
+            if(login.username == null || login.password == null)
+            {
+                var err = "Fejl ved login: Brugernavn eller adgangskode er null.";
+                _logger.LogError(err);
+                return StatusCode(StatusCodes.Status400BadRequest, err);
+            }
+
+            //lav test og logging
+            if(secret == null || issuer == null)
+            {
+                var err = "Fejl ved login: Der opstod en fejl ved login.";
+                _logger.LogError(err);
+                return StatusCode(StatusCodes.Status500InternalServerError, err);
+            }
+
             if (login != null && !string.IsNullOrEmpty(login.username) && !string.IsNullOrEmpty(login.password))
             {
                 if (login.username == "admin" && login.password == "1234")
                 {
                     try
                     {
-                        // Hent hemmeligheden og udstederen fra Vault
-                        var secret = await _vaultService.GetSecretAsync("secrets", "SecretKey");
-                        var issuer = await _vaultService.GetSecretAsync("secrets", "IssuerKey");
-
                         // Generer JWT-token med hemmeligheden og udstederen fra Vault
                         var token = GenerateJwtToken(login.username, issuer, secret);
+
+                        // Log IP-adressen
+                        LogIPAddress();
 
                         // Returner JWT-tokenet som svar
                         return Ok(new { token });
@@ -81,6 +108,7 @@ namespace Authentication.Controllers
                     catch (Exception ex)
                     {
                         _logger.LogError("Fejl ved generering af JWT-token: {0}", ex.Message);
+                        _nLogger.Error(ex, "Fejl ved generering af JWT-token");
                         return StatusCode(StatusCodes.Status500InternalServerError, "Der opstod en fejl under login.");
                     }
                 }
@@ -89,17 +117,38 @@ namespace Authentication.Controllers
             return Unauthorized();
         }
 
+        private void LogIPAddress()
+        {
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = System.Net.Dns.GetHostAddresses(hostName);
+            var ipAddr = ips.FirstOrDefault(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)?.ToString();
 
+            if (!string.IsNullOrEmpty(ipAddr))
+            {
+                _logger.LogInformation($"XYZ Service responding from {ipAddr}");
+                _nLogger.Info($"XYZ Service responding from {ipAddr}");
+            }
+            else
+            {
+                _logger.LogWarning("Unable to retrieve the IP address.");
+                _nLogger.Warn("Unable to retrieve the IP address.");
+            }
+        }
 
         [HttpGet("get-secret")]
         public async Task<IActionResult> GetSecret()
         {
-            var secret = await _vaultService.GetSecretAsync("secret", "mySecret");
-            return Ok(new { mySecret = secret });
+            try
+            {
+                var secret = await _vaultService.GetSecretAsync("secret", "mySecret");
+                return Ok(new { mySecret = secret });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Fejl ved hentning af hemmelighed: {0}", ex.Message);
+                _nLogger.Error(ex, "Fejl ved hentning af hemmelighed");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Der opstod en fejl under hentning af hemmelighed.");
+            }
         }
-
-
-
-
     }
 }
