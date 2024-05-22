c915d51f9083823fe201c079d39d68dc7a5733c1
jakobhaulund <jakobhaulund@hotmail.com>
2024-05-22 13:04:19 +0200

    pls virk

--- AuthenticationService/Controllers/Authenticationcontroller.cs
+++ AuthenticationService/Controllers/Authenticationcontroller.cs
@@ -149,34 +149,5 @@ namespace Authentication.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "Der opstod en fejl under hentning af hemmelighed.");
             }
         }
-
-        [HttpGet("authorized")]
-        [Authorize]
-        public async Task<IActionResult> GetAuthorized()
-        {
-            return Ok("You are authorized");
-        }
-
-        [HttpGet("unauthorized")]
-        public async Task<IActionResult> GetUnauthorized()
-        {
-            return Unauthorized("You're not authorized");
-        }
-
-        [HttpGet("user")]
-        [Authorize(Roles = "1")]
-        public async Task<IActionResult> GetUser()
-        {
-            _logger.LogInformation("GetUser called");
-            return Ok("You are a normal user");
-        }
-
-        [HttpGet("admin")]
-        [Authorize(Roles = "2")]
-        public async Task<IActionResult> GetAdmin()
-        {
-            _logger.LogInformation("GetAdmin called");
-            return Ok("You are an admin");
-        }
     }
 }
