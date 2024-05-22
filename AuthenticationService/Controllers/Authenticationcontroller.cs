0d47800075636a71c3c31ae33bb307645c04ccd2
jakobhaulund <jakobhaulund@hotmail.com>
2024-05-22 13:21:52 +0200

    tester

--- AuthenticationService/Controllers/Authenticationcontroller.cs
+++ AuthenticationService/Controllers/Authenticationcontroller.cs
@@ -116,6 +116,35 @@ namespace Authentication.Controllers
             }
         }
 
+        [HttpGet("authorized")]
+        [Authorize]
+        public async Task<IActionResult> GetAuthorized()
+        {
+            return Ok("You are authorized");
+        }
+
+        [HttpGet("unauthorized")]
+        public async Task<IActionResult> GetUnauthorized()
+        {
+            return Unauthorized("You're not authorized");
+        }
+
+        [HttpGet("user")]
+        [Authorize(Roles = "1")]
+        public async Task<IActionResult> GetUser()
+        {
+            _logger.LogInformation("GetUser called");
+            return Ok("You are a normal user");
+        }
+
+        [HttpGet("admin")]
+        [Authorize(Roles = "2")]
+        public async Task<IActionResult> GetAdmin()
+        {
+            _logger.LogInformation("GetAdmin called");
+            return Ok("You are an admin");
+        }
+
         private void LogIPAddress()
         {
             var hostName = System.Net.Dns.GetHostName();
