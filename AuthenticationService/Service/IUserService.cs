4bfeb72f271df7fe1cd56433842ecabfec75e508
jakobhaulund <jakobhaulund@hotmail.com>
2024-05-23 15:43:15 +0200

    aoskdajsd

--- AuthenticationService/Service/IUserService.cs
+++ AuthenticationService/Service/IUserService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Authentication.Models;
+using Models;
 
 namespace Authentication.Service
 {
     public interface IUserService
     {
         Task<HttpResponseMessage> GetUserAsync(Guid _id);
-        Task<bool> ValidateUser(User user);
+        Task<bool> ValidateUser(LoginDTO user); // lav så den returnerer en bruger så vi kan fiske rolle ud og bruge i login validate
     }
 }
